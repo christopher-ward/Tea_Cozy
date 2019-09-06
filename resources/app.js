ec3716d53652edf7603e74adb8693cb46d3640de
chrisward90 <chrsward90@gmail.com>
2019-09-06 12:59:25 -0700

    Added more jQuery including slick plugin objects.

--- resources/app.js
+++ resources/app.js
@@ -6,17 +6,48 @@
 //     document.getElementsByClassName("mobile-list")[0].id = "";
 //   }
 // });
+$(document).ready(function(){
+  $("#nav-toggle").on("click", function(event){
+    event.preventDefault();
+    $(".mobile-list").slideToggle();
+    $(this).toggleClass("active");
+  });
 
-$("#nav-toggle").on("click", function(event){
-  event.preventDefault();
-  $(".mobile-list").slideToggle();
-  $(this).toggleClass("active");
-});
+  $(".mobile-list a").on("click", function(event){
+    event.preventDefault();
+    var headerHeight = $("header").height();
+    var id = $(this).attr("href");
+    var originalScrollCoordinate = $(id).offset().top;
+    var newScrollCoordinate = originalScrollCoordinate - headerHeight;
+    $("html").animate({
+      scrollTop : newScrollCoordinate
+    });
+  });
 
-$(".mobile-list a").on("click", function(){
-  var headerHeight = $("header").height();
-  var id = $(this).attr("href");
-  var originalScrollCoordinate = $(id).offset().top;
-  var newScrollCoordinate = originalScrollCoordinate - headerHeight;
-  $("html").scrollTop(newScrollCoordinate);
+  // Slick Carousel
+  var $galleryImg = $(".gallery img");
+  var $closeBtn = $(".btn");
+  var $carousel = $(".carousel");
+  var $overlay = $(".overlay");
+
+  $carousel.slick({
+    centerMode: true,
+    fade: true,
+    arrows: false,
+    dots: true,
+    draggable: false
+  });
+
+  $galleryImg.on("click", function(click){
+    var $imgId = $(this).attr("id");
+    // Initiate fade-in / set display: block
+    $overlay.fadeIn();
+    // Go to slide with clicked image and ensure proper dimensions and position on page
+    $carousel.slick("slickGoTo", $imgId, true).slick("setPosition");
+  });
+
+  $closeBtn.on("click", function(event){
+    event.preventDefault();
+    $overlay.fadeOut();
+  });
 });
